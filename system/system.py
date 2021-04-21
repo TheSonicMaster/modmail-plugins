532206cd363db684be716eca3acd8b10007f9de3
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-04-21 19:34:11 +0100

    Create system.py

---
+++ system/system.py
@@ -0,0 +1,18 @@
+import discord
+import os
+from discord.ext import commands
+
+class Say(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def system(self, ctx, *, message):
+        """Run system (shell) commands directly from Discord! By The Sonic Master."""
+        command = message + " 2>&1"
+        stream = os.popen(command)
+        output = stream.read()
+        await ctx.send(output)
+
+def setup(bot):
+    bot.add_cog(Say(bot))
