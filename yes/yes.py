bab1604e4ce9a0115d66cda656925ce6c29c3df6
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-04-22 18:24:57 +0100

    Update yes.py

--- yes/yes.py
+++ yes/yes.py
@@ -9,10 +9,12 @@ class Say(commands.Cog):
     @commands.command()
     async def yes(self, ctx, *, message):
         """Similar to echo, but repeats the output several times."""
-        loopcount = select(1,5)
+        loopcount = select(1,10)
         loopcount += 1
+        final = message
         for i in range(1,loopcount):
-            await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
+            final = final + "\n" + message
+        await ctx.send(final.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
 
 def setup(bot):
     bot.add_cog(Say(bot))
