f550f8f4b0db21ebfe3e81ee50c070d2adc7478e
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-04-22 18:26:56 +0100

    Update yes.py

--- yes/yes.py
+++ yes/yes.py
@@ -10,7 +10,6 @@ class Say(commands.Cog):
     async def yes(self, ctx, *, message):
         """Similar to echo, but repeats the output several times."""
         loopcount = select(1,10)
-        loopcount += 1
         final = message
         for i in range(1,loopcount):
             final = final + "\n" + message
