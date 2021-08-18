1fabd13dcafb693e963c3bcdab6b33dfd000d2b6
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-08-18 11:52:19 +0100

    Update echo.py

--- echo/echo.py
+++ echo/echo.py
@@ -8,7 +8,7 @@ class Say(commands.Cog):
     @commands.command()
     async def echo(self, ctx, *, message):
         """Echos your message back to you!"""
-        await ctx.send("Message sent by + " ctx.author + "using `+echo` command:\n" + message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
+        await ctx.send("Message sent by + " str(ctx.author) + "using `+echo` command:\n" + message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
 
 def setup(bot):
     bot.add_cog(Say(bot))
