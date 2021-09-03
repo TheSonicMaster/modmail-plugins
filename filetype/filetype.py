1e53b1f5124758a3ace4997b04ab8a2c1bf15d37
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-09-03 14:58:02 +0100

    Update filetype.py

--- filetype/filetype.py
+++ filetype/filetype.py
@@ -2,6 +2,7 @@ import discord
 import os
 from discord.ext import commands
 from datetime import datetime
+from requests import get
 
 class filetype(commands.Cog):
     """Detect file type of attached files."""
@@ -11,11 +12,15 @@ class filetype(commands.Cog):
     @commands.group(invoke_without_command=True)
     @commands.Cog.listener()
     async def on_message(self, ctx, *, message):
+        if message.attachments:
+            await ctx.send("Checking file type of attached file...")
         attachment = message.attachments[0]
         url = attachment.url
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        file = "/tmp/" + timestamp
-        os.popen("curl -Ls " + url + " -o " + file)
+        file = "/tmp/" + str(timestamp)
+        downloaded = get(url)
+        with open(file,"wb") as download:
+            download.write(downloaded.content)
         stream = os.popen("file " + file)
         output = stream.read()
         await ctx.send(output)
