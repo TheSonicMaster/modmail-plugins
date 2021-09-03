f0f263cd7a84a9a55a7b364b76709540d31c899e
The Sonic Master <51241115+TheSonicMaster@users.noreply.github.com>
2021-09-03 14:21:35 +0100

    Create filetype.py

---
+++ filetype/filetype.py
@@ -0,0 +1,22 @@
+import discord
+import os
+from discord.ext import commands
+from datetime import datetime
+
+class filetype(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def filetype(self, ctx):
+        """Detect file type of attached files."""
+        attachment = ctx.message.attachments[0]
+        url = attachment.url
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        file = "/tmp/" + timestamp
+        os.popen("curl -Ls " + url + " -o " + file)
+        stream = os.popen("file " + file)
+        output = stream.read()
+        print(output)
+def setup(bot):
+    bot.add_cog(Say(bot))
