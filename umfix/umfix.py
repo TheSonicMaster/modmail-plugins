698db55d9ec9371e46a9633f10bc17d6683a830b
clerickx <72374250+clerickx@users.noreply.github.com>
2021-11-21 02:09:01 +0530

    Create umfix.py

---
+++ umfix/umfix.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+
+class umfix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.id == 735550905721684098:
+            words = message.content.lower().split(" ")
+            for word in words:
+                if i.startswith("uh"):
+                    await message.delete()
+                    await message.author.send("Hey you used uh again stop it")
+                elif i.startswith("um"):
+                    await message.delete()
+                    await message.author.send("Hey you used um again stop it")
+                    
+ 
+def setup(bot):
+    bot.add_cog(stafftime(bot))
