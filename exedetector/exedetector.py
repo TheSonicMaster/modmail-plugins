73902612b747e1914f7ba60c28cc6b7ee8584c84
clerickx <72374250+clerickx@users.noreply.github.com>
2021-11-21 18:22:19 +0530

    Update exedetector.py

--- exedetector/exedetector.py
+++ exedetector/exedetector.py
@@ -12,11 +12,11 @@ class exedetector(commands.Cog):
         if message.attachments:
             for attachment in message.attachments:
                 if attachment.filename.lower().endswith(".exe"):
-					          await message.delete()
-					          exe = Embed(color=self.bot.main_color, description="**Exe detected at **" + message.channel.mention + "\n" + attachment.url)
-					          exe.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-					          exe.timestamp = datetime.datetime.utcnow()
-					          await self.bot.get_channel(611613073039687701).send(embed=exe)
+		    await message.delete()
+	            exe = Embed(color=self.bot.main_color, description="**Exe detected at **" + message.channel.mention + "\n" + attachment.url)
+		    exe.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+		    exe.timestamp = datetime.datetime.utcnow()
+	            await self.bot.get_channel(611613073039687701).send(embed=exe)
 
 def setup(bot):
     bot.add_cog(exedetector(bot))
