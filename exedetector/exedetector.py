977219de4a80670843e6c2d1f78f5b45033cbda7
clerickx <72374250+clerickx@users.noreply.github.com>
2021-11-21 18:28:50 +0530

    Update exedetector.py

--- exedetector/exedetector.py
+++ exedetector/exedetector.py
@@ -13,6 +13,7 @@ class exedetector(commands.Cog):
             for attachment in message.attachments:
                 if attachment.filename.lower().endswith(".exe"):
 		    await message.delete()
+		    await message.channel.send(f"{message.author.mention} Please do not send executable files. You may send a link to the website/webpage where they may be obtained from instead.", delete_after=3)
 	            exe = Embed(color=self.bot.main_color, description="**Exe detected at **" + message.channel.mention + "\n" + attachment.url)
 		    exe.set_author(name=message.author.name, icon_url=message.author.avatar_url)
 		    exe.timestamp = datetime.datetime.utcnow()
