893d03e4cfeaa672e5dd26a61fcad16956e30726
Captain riggs <72374250+clerickx@users.noreply.github.com>
2022-02-18 20:34:41 +0530

    Removed removed staffs
    :cry:

--- stafftime/stafftime.py
+++ stafftime/stafftime.py
@@ -14,12 +14,10 @@ class stafftime(commands.Cog):
             if "<@&581196949383020584>" in message.content.lower():
                 thesonicmaster = datetime.datetime.now(pytz.timezone('Europe/London')).strftime("%H:%M (%I:%M %p)")
                 captain = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%H:%M (%I:%M %p)")
-                lifeiscombo = datetime.datetime.now(pytz.timezone('Africa/Cairo')).strftime("%H:%M (%I:%M %p)")
                 minecat = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M (%I:%M %p)")
                 staff=discord.Embed(color=self.bot.main_color, title="Staff timings", url="https://github.com/clerickx/modmail-plugins/tree/master/stafftime")
                 staff.add_field(name="TheSonicMaster", value=thesonicmaster, inline=True)
                 staff.add_field(name="Captain riggs:tm:", value=captain, inline=True)
-                staff.add_field(name="LifeIsCombo", value=lifeiscombo, inline=True)
                 staff.add_field(name="MinecatMeow", value=minecat, inline=True)
                 await message.channel.send(embed=staff)
             elif "<@494884004068327425>" in message.content.lower():
@@ -34,12 +32,6 @@ class stafftime(commands.Cog):
             elif "<@!735550905721684098>" in message.content.lower():
                 captain = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%H:%M (%I:%M %p)")
                 await message.channel.send(f"The current time of Captain riggs:tm: is {captain}. Depending on this time you may need to be patient for a response.")
-            elif "<@536654456968839215>" in message.content.lower():
-                lifeiscombo = datetime.datetime.now(pytz.timezone('Africa/Cairo')).strftime("%H:%M (%I:%M %p)")
-                await message.channel.send(f"The current time of LifeIsCombo is {lifeiscombo}. Depending on this time you may need to be patient for a response.")
-            elif "<@!536654456968839215>" in message.content.lower():
-                lifeiscombo = datetime.datetime.now(pytz.timezone('Africa/Cairo')).strftime("%H:%M (%I:%M %p)")
-                await message.channel.send(f"The current time of LifeIsCombo is {lifeiscombo}. Depending on this time you may need to be patient for a response.")
             elif "<@845406066505285642>" in message.content.lower():
                 minecat = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M (%I:%M %p)")
                 await message.channel.send(f"The current time of MinecatMeow is {minecat}. Depending on this time you may need to be patient for a response.")
@@ -51,12 +43,10 @@ class stafftime(commands.Cog):
     async def stafftime(self, ctx):
         thesonicmaster = datetime.datetime.now(pytz.timezone('Europe/London')).strftime("%H:%M (%I:%M %p)")
         captain = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%H:%M (%I:%M %p)")
-        lifeiscombo = datetime.datetime.now(pytz.timezone('Africa/Cairo')).strftime("%H:%M (%I:%M %p)")
         minecat = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%H:%M (%I:%M %p)")
         staff=discord.Embed(color=self.bot.main_color, title="Staff timings", url="https://github.com/clerickx/modmail-plugins/tree/master/stafftime")
         staff.add_field(name="TheSonicMaster", value=thesonicmaster, inline=True)
         staff.add_field(name="Captain riggs:tm:", value=captain, inline=True)
-        staff.add_field(name="LifeIsCombo", value=lifeiscombo, inline=True)
         staff.add_field(name="MinecatMeow", value=minecat, inline=True)
         await ctx.send(embed=staff)
 
