b19f9d2ac9c7d52f55f2b2f4348cac7bed6273e6
Captain riggs <72374250+clerickx@users.noreply.github.com>
2022-02-19 19:11:03 +0530

    Patch on_message_edit
    This patch will detect edited message links

--- ipgrabdetector/ipgrabdetector.py
+++ ipgrabdetector/ipgrabdetector.py
@@ -45,6 +45,18 @@ class ipgrabdetector(commands.Cog):
                 grabber.timestamp = datetime.utcnow()
                 await self.bot.get_channel(611613073039687701).send(embed=grabber)
                 break
+                
+    @commands.Cog.listener()
+    async def on_message_edit(before, after):
+        for link in links:
+            if link in message.content:
+                await message.delete()
+                await message.channel.send(f"{message.author.mention} Your message has been deleted for containing an IP grabber link.", delete_after=3)
+                grabber = Embed(color=self.bot.main_color, description="**IP grabber link detected at** " + message.channel.mention + "\n" + link)
+                grabber.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+                grabber.timestamp = datetime.utcnow()
+                await self.bot.get_channel(611613073039687701).send(embed=grabber)
+                break
 
 def setup(bot):
     bot.add_cog(ipgrabdetector(bot))
