16127ef31f0d862bafacd2fe2188e02c1c3a2e12
The Sonic Master <supdroidgamingdivision@gmail.com>
2022-06-04 18:58:12 +0100

    More channel ignores

--- leveling/leveling.py
+++ leveling/leveling.py
@@ -27,7 +27,7 @@ class Leveling(Cog):
     async def on_message(self, message: Message) -> None:
         if message.author.bot:
             return
-        if message.channel.id == 671069603048325131 or message.channel.id == 804741038040285224:
+        if message.channel.id == 583379256441045012 or message.channel.id == 671069603048325131 or message.channel.id == 804741038040285224:
             return
         if isinstance(message.channel, discord.channel.DMChannel):
             return
