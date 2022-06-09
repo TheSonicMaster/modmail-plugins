d627d4749925c98678e0084925197d3e3b658b57
Captain riggs <72374250+clerickx@users.noreply.github.com>
2022-06-09 23:03:23 +0530

    Update stafftime.py

--- stafftime/stafftime.py
+++ stafftime/stafftime.py
@@ -4,11 +4,11 @@ import datetime
 
 from discord.ext import commands
 
-def getStaffEmbed():
+def getStaffEmbed(self):
     staffeb=discord.Embed(color=self.bot.main_color, title="Staff timings", url="https://github.com/clerickx/modmail-plugins/tree/master/stafftime")
     staffeb.add_field(name="TheSonicMaster", value=getTime('Europe/London'), inline=True)
     staffeb.add_field(name="Captain riggs:tm:", value=getTime('Asia/Kolkata'), inline=True)
-    staffeb.add_field(name="MinecatMeow", value=getTime('US/Eastern'), inline=True)
+    staffeb.add_field(name="Xinc", value=getTime('Europe/London'), inline=True)
     return staffeb
         
 def getTime(timezone):
@@ -20,22 +20,24 @@ class stafftime(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot:
-            if "<@&581196949383020584>" in message.content.lower():
-                await message.channel.send(embed=getStaffEmbed())
-            elif "<@!494884004068327425" in message.content.lower():
-                thesonicmaster = getTime('Europe/London')
-                await message.channel.send(f"Please do not ping The Sonic Master. It is considered rude and will not make The Sonic Master respond any faster, especially if The Sonic Master is offline. Be patient for a response and do not expect one immediately.\n\nThe current time of The Sonic Master is {thesonicmaster} (Timezones Exist).")
-            elif "<@!735550905721684098>" in message.content.lower():
-                captain = getTime('Asia/Kolkata')
-                await message.channel.send(f"The current time of Captain riggs:tm: is {captain}. Depending on this time you may need to be patient for a response.")
-            elif "<@!845406066505285642>" in message.content.lower():
-                minecat = getTime('US/Eastern')
-                await message.channel.send(f"The current time of MinecatMeow is {minecat}. Depending on this time you may need to be patient for a response.")
+        if message.author.bot:
+            return
+        
+        if "<@&581196949383020584>" in message.content.lower():
+            await message.channel.send(embed=getStaffEmbed())
+        elif "<@!494884004068327425>" in message.content.lower():
+            thesonicmaster = getTime('Europe/London')
+            await message.channel.send(f"Please do not ping The Sonic Master. It is considered rude and will not make The Sonic Master respond any faster, especially if The Sonic Master is offline. Be patient for a response and do not expect one immediately.\n\nThe current time of The Sonic Master is {thesonicmaster} (Timezones Exist).")
+        elif "<@!735550905721684098>" in message.content.lower():
+            captain = getTime('Asia/Kolkata')
+            await message.channel.send(f"The current time of Captain riggs:tm: is {captain}. Depending on this time you may need to be patient for a response.")
+        elif "<@!775023869261381643>" in message.content.lower():
+            xinc = getTime('Europe/London')
+            await message.channel.send(f"The current time of Xinc is {xinc}. Depending on this time you may need to be patient for a response.")
     
     @commands.command()
     async def stafftime(self, ctx):
-        await ctx.send(embed=getStaffEmbed())
+        await ctx.send(embed=getStaffEmbed(self))
 
 def setup(bot):
     bot.add_cog(stafftime(bot))
